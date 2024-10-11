2f2341bc64a029037759c0d34507f9eda00948b4
lvisnaw <lvisnaw@gmail.com>
2024-10-10 19:23:14 -0600

    Completing week 4 prepare activities.

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require("../utilities/account-validation")
 
 // Route for the "My Account" page
 router.get("/", utilities.handleErrors(accountController.buildAccountView))
@@ -13,6 +14,11 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Route for the register page
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+    "/register", 
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router
